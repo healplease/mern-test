6aaaaabc6df153700be6240651e393b7b017bbf5
healplease <gavaaalex2012@gmail.com>
2024-08-17 19:02:22 +0200

    todo model cleanup

--- backend/src/todo/models.js
+++ backend/src/todo/models.js
@@ -1,7 +1,5 @@
 import mongoose from "mongoose";
 
-import { Task } from "./task/models.js";
-
 export const TodoSchema = new mongoose.Schema({
   userId: {
     type: mongoose.Schema.Types.ObjectId,
@@ -32,19 +30,6 @@ TodoSchema.virtual("user", {
 TodoSchema.set("toObject", { virtuals: true });
 TodoSchema.set("toJSON", { virtuals: true });
 
-// TodoSchema.virtual("completed").get(function () {
-//   return this.tasks.length > 0 ? this.tasks.every(task => task.completed) : false;
-// });
-
-// TodoSchema.virtual("percentCompleted").get(function () {
-//   if (this.tasks.length === 0) {
-//     return 0;
-//   }
-
-//   const completedTasks = this.tasks.filter(task => task.completed);
-//   return Math.round((completedTasks.length / this.tasks.length) * 100);
-// });
-
 TodoSchema.methods.toJSON = function () {
   return {
     id: this._id,
@@ -52,8 +37,6 @@ TodoSchema.methods.toJSON = function () {
     title: this.title,
     description: this.description,
     tasks: this.tasks,
-    // completed: this.completed,
-    // percentCompleted: this.percentCompleted,
     createdAt: this.createdAt,
     updatedAt: this.updatedAt,
   };
